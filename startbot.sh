1f940047eb52f985981cea4ad9859d39c782b0ae
MadeWiguna <79256327+MadeWiguna@users.noreply.github.com>
2021-08-20 11:07:19 +0800

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -7,12 +7,41 @@
 ## restart the bot by turning the worker off and back on
 
 echo "Creating requirements.txt..."
-cd /home/Nyaa-Telegram-Bot
+cd /home/wiguna-cermin
 cat > requirements.txt << EOF
 {
-pyrogram
 aiohttp
+anytree
+aria2p
+appdirs
+beautifulsoup4
+cloudscrape
+feedparser
+gitpython
+google-api-python-client
+google-auth-httplib2
+google-auth-oauthlib
+gunicorn
+heroku3
+js2py
+lk21
+lxml
+psutil
+psycopg2-binary
+pybase64
+pyrogram
+python-dotenv
+python-magic
+python-telegram-bot
+qbittorrent-api
+requests
+speedtest-cli
+telegraph
+tenacity
 TgCrypto
+torrentool
+urllib3
+youtube_dl
 }
 EOF
 echo "Done."
